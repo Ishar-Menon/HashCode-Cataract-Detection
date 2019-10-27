441d33294c7207f19705b11079a3feb4498a2e9a
Ishar-Menon <ishar1999messi@gmail.com>
2019-10-27 06:42:37 +0530

    Front-end clean up

--- Android/app/src/main/java/com/e/ishar/front_end/ResultActivity.java
+++ Android/app/src/main/java/com/e/ishar/front_end/ResultActivity.java
@@ -3,6 +3,7 @@ package com.e.ishar.front_end;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,14 +13,34 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        TextView txt1 = (TextView)findViewById(R.id.res);
-        TextView txt2 = (TextView)findViewById(R.id.prob);
+        TextView result = (TextView)findViewById(R.id.res);
+        TextView probability = (TextView)findViewById(R.id.prob);
+        TextView conclu = (TextView)findViewById(R.id.result);
+        TextView symp = (TextView)findViewById(R.id.symptoms);
         Intent intent = getIntent();
         String res = intent.getStringExtra("Response");
         String prob = intent.getStringExtra("Probability");
-        txt1.setText("Result "+ res);
-        txt2.setText("Probability "+prob);
-        Toast.makeText(this, "Result activity " + res + " " + prob, Toast.LENGTH_SHORT).show();
+        result.setText("Result : "+ res);
+        probability.setText("Probability : "+prob);
+        double decide = Double.parseDouble(res);
+        double score = Double.parseDouble(prob.substring(0,5));
+        double mid;
+        String end;
+        if(decide == 1)
+        {
+            mid = score*100;
+            end = "have cataract";
+            symp.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            mid = 100 - (score*100);
+            end = "do not have cataract";
+        }
+
+        String finalStr = "We are " + mid+ "% confident that you "+end;
+        conclu.setText(finalStr);
+//        Toast.makeText(this, "Result activity " + res + " " + prob, Toast.LENGTH_SHORT).show();
     }
 
 }
